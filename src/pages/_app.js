45f9428b1726a6b665dbaf1f08fe9295096f7622
Lucia Maake <luciamaake4@gmail.com>
2026-01-01 02:58:43 +0200

    Deploy: full site horizontal scroll + sticky navbar

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,17 +1,13 @@
-// pages/_app.js
-import "@/styles/globals.css";
-import Navbar from "@/components/Navbar";
+import "../styles/globals.css";
+import Navbar from "../components/Navbar";
+import Footer from "../components/Footer";
 
-export default function App({ Component, pageProps }) {
+export default function MyApp({ Component, pageProps }) {
   return (
-    <div className="min-w-[1280px] overflow-x-auto">
-      {/* Sticky Navbar */}
+    <>
       <Navbar />
-
-      {/* Main content with top padding so it doesn't hide behind navbar */}
-      <main className="pt-[72px]">
-        <Component {...pageProps} />
-      </main>
-    </div>
+      <Component {...pageProps} />
+      <Footer />
+    </>
   );
 }
